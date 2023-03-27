71a4c94924a269d8ecefd15445f227660b36bd02
Daniel-J-94 <danieljowett@outlook.com>
2023-03-28 00:20:55 +0100

    commits for step 3

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 const express = require("express");
 const { getCategories } = require("./controllers/getCategoriesController");
-const { getReviewById } = require("./controllers/getReviewByIdController");
-const { getReviews } = require("./controllers/getReviewsController");
 const app = express();
 
 app.get("/api/categories", getCategories);
-app.get("/api/reviews/:review_id", getReviewById);
-app.get("/api/reviews", getReviews);
+
 app.use("*", (req, res) => {
   res.status(404).send({ message: "Doesn't exist" });
 });
 app.use((err, req, res, next) => {
   console.log(err);
-  res.status(404).send({ message: "No review with that ID" });
+  res.status(404).send({ message: "Doesn't exist" });
 });
 module.exports = app;
