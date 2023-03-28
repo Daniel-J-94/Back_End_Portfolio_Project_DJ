65962de7ee50c72a22a4ef1b669e30e1769c22d3
Daniel-J-94 <danieljowett@outlook.com>
2023-03-28 14:58:41 +0100

    commits for ticket 6, refactor db query to use

--- models/getCommentsByReviewIdModel.js
+++ models/getCommentsByReviewIdModel.js
@@ -8,7 +8,9 @@ exports.getterCommentsByReviewId = (revIdFromReq) => {
   const revIdNumber = revIdFromReq.review_id;
 
   return db
-    .query(`SELECT * FROM comments WHERE comments.review_id = ${revIdNumber}`)
+    .query(`SELECT * FROM comments WHERE comments.review_id = $1`, [
+      revIdNumber,
+    ])
     .then((result) => {
       return result.rows;
     });
