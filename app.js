9b09e83e6e2b7aa96b1ab09a0b232eff3bac3666
Daniel-J-94 <danieljowett@outlook.com>
2023-03-28 13:50:30 +0100

    commits for ticket 3 to reflect error handling feedback on other tickets and avoid merge conflicts

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ app.use("*", (req, res) => {
 });
 app.use((err, req, res, next) => {
   console.log(err);
-  res.status(404).send({ message: "Doesn't exist" });
+  res.status(500).send({ message: "Server error" });
 });
 module.exports = app;
