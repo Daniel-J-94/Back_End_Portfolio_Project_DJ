953e0819b92b39939a915a14fbb5dd140f9b98a5
Daniel-J-94 <danieljowett@outlook.com>
2023-03-28 14:55:57 +0100

    Commits for ticket 4, refactor sql query to use

--- models/getReviewByIdModel.js
+++ models/getReviewByIdModel.js
@@ -3,14 +3,17 @@ const db = require("../db/connection");
 exports.fetchReviewById = (revIdFromReq) => {
   const revIdNumber = revIdFromReq.review_id;
 
-  return db
-    .query(`SELECT * FROM reviews WHERE review_id = ${revIdNumber};`)
-    .then((result) => {
-      console.log("model result:", result);
-      if (result.rows.length === 0) {
-        return err;
-      } else {
-        return result.rows[0];
-      }
-    });
+  return (
+    db
+      .query("SELECT * FROM reviews WHERE review_id = $1;", [revIdNumber])
+      // .query(`SELECT * FROM reviews WHERE review_id = ${revIdNumber};`)
+      .then((result) => {
+        console.log("model result:", result);
+        if (result.rows.length === 0) {
+          return err;
+        } else {
+          return result.rows[0];
+        }
+      })
+  );
 };
