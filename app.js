c22d6715bd3ea8f61da1dd3b9457846bfff13c11
Daniel-J-94 <danieljowett@outlook.com>
2023-03-28 13:52:31 +0100

    commits for ticket 5 to reflect feedback on error handling

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.use("*", (req, res) => {
 });
 app.use((err, req, res, next) => {
   console.log(err);
-  res.status(404).send({ message: "No review with that ID" });
+  res.status(500).send({ message: "Server error" });
 });
 module.exports = app;
