317ffb8ce7cc9b1a31a445df20df2d7f0fee8f42
Daniel-J-94 <danieljowett@outlook.com>
2023-03-28 17:48:59 +0100

    housekeeping post 5 merge

--- controllers/getReviewByIdController.js
+++ controllers/getReviewByIdController.js
@@ -2,10 +2,12 @@ const { fetchReviewById } = require("../models/getReviewByIdModel.js");
 
 exports.getReviewById = (req, res, next) => {
   const revIdFromReq = req.params.review_id;
+
   fetchReviewById(revIdFromReq)
     .then((result) => {
       res.status(200).send(result);
     })
+
     .catch((err) => {
       next(err);
     });
