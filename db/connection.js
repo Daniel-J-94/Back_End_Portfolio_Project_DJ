1fc89ca78d8728ffd099487828ff4a35b948ce3c
Daniel-J-94 <danieljowett@outlook.com>
2023-03-31 08:40:46 +0100

    db connected to elephant, will host app once branch is merged into main

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,21 @@
 const { Pool } = require("pg");
+
 const ENV = process.env.NODE_ENV || "development";
 
 require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error("PGDATABASE not set");
+if (!process.env.PGDATABASE && !process.env.DB_URL) {
+  throw new Error("PGDATABASE or DB_ URL not set");
 }
 
-module.exports = new Pool();
+const config =
+  process.env.NODE_ENV === "production"
+    ? {
+        connectionString: process.env.DB_URL,
+        max: 2,
+      }
+    : {};
+
+module.exports = new Pool(config);
