02f3423a20d2432cb51a176c91fe3813e114e6de
Daniel-J-94 <danieljowett@outlook.com>
2023-03-31 11:01:35 +0100

    controller refactor based on feedback

--- controllers/deleteCommentController.js
+++ controllers/deleteCommentController.js
@@ -4,7 +4,7 @@ exports.deleteComment = (req, res, next) => {
   const id = req.params.comment_id;
   removeComment(id)
     .then((result) => {
-      res.status(204).send(result.body);
+      res.status(204).send(res.sendStatus(204));
     })
     .catch((err) => {
       next(err);
